70c58fbe4663f8db05f589efe08b61c6b4a3a3f4
Jeremy Williams <jeremy.williams@paytm.com>
2019-09-13 14:15:19 -0400

    Remove redundant pixel_loop

--- src/app.c
+++ src/app.c
@@ -21,23 +21,6 @@
 #define BLOCK_SIZE 8
 #define MAJOR_BLOCK_SIZE 16
 
-#define pixel_loop(src_x, src_y, src_pitch, dst_x, dst_y, dst_pitch, w, h) \
-  for ( \
-    int y_ = 0, \
-        x_ = 0, \
-        i_ = 0, \
-        si = src_y * src_pitch + src_x, \
-        di = dst_y * dst_pitch + dst_x; \
-\
-    i_ < w * h; \
-\
-    ++i_, \
-    x_ = i_ % w, \
-    y_ = i_ / w, \
-    si = (y_ + src_y) * src_pitch + (x_ + src_x), \
-    di = (y_ + dst_y) * dst_pitch + (x_ + dst_x) \
-  )
-
 typedef struct {
   int bpp;
   int rmask;
