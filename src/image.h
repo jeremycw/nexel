9ca35b51e4ea607d8cb57bb2c7cc2709c4eea202
Jeremy Williams <jeremy.williams@paytm.com>
2019-09-13 14:04:29 -0400

    Remove unused code

--- src/image.h
+++ src/image.h
@@ -18,8 +18,6 @@
     di = (y_ + dst_y) * dst_pitch + (x_ + dst_x) \
   )
 
-void double_px(const int* in, int n, int pitch, int* out);
-void half_px(const int* in, int n, int pitch, int* out);
 void alpha_channel_to_rgba(unsigned char* in, unsigned int* out, int n, unsigned int rgb);
 void rotate_clockwise(unsigned int* in, int w, int h, unsigned int* out);
 void mirror_horizontal(unsigned int* in, int w, int h);
